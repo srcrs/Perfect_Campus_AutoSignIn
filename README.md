a7a12e1905cd3a04c54df39e00a138b022fb451a
srcrs <srcrs@foxmail.com>
2020-10-29 09:57:15 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# XYU-AutoSignIn
+## XYU-AutoSignIn
+
+<div align="center"> 
+<h1 align="center">信阳学院完美校园签到</h1>
+<img src="https://img.shields.io/github/issues/srcrs/XYU-AutosignIn?color=green">
+<img src="https://img.shields.io/github/stars/srcrs/XYU-AutosignIn?color=yellow">
+<img src="https://img.shields.io/github/forks/srcrs/XYU-AutosignIn?color=orange">
+<img src="https://img.shields.io/github/license/srcrs/XYU-AutosignIn?color=ff69b4">
+</div>
+
+# 简介
+
+信阳学院完美校园每日自动签到，从此让你解放双手，支持多用户批量签到。
+
+# 功能
+
+- 完美校园签到
+
+- 支持多用户批量签到
+
+- 支持推送运行结果至微信(使用server酱)
+
+- 随机温度，随机经纬度(在合适的范围内)
+
+# 使用方法
+
+## 准备
+
+- 完美校园APP账号(需要手机号和密码,若未使用需注册)
+
+- 所在学院、专业、班级(可在支付宝完美校园小程序健康打卡页面复制)
+
+- 若需推送至微信，请注册server酱，并获取其SCKEY
+
+## 1.fork本项目
+
+## 2.开启antions(默认你的actions是处于禁用状态)
+
+![](assets/img/开启actions.gif)
+
+## 3.将个人信息添加至Secrets
+
+Name - Value
+-|-
+USERS | 手机号,密码,学院-专业-班级,监护人手机号,SCKEY
+
+多用户的Value格式如下：
+
+```sh
+手机号1,密码1,学院-专业-班级1,监护人手机号1,SCKEY
+手机号2,密码2,学院-专业-班级2,监护人手机号2,
+手机号3,密码3,学院-专业-班级3,监护人手机号3,
+                    .
+                    .
+                    .
+```
+
+如需使用 [server酱](http://sc.ftqq.com/) 推送至微信，按照官网教程注册，获取其`SCKEY`。只需要在第一个用户的后面添加即可。
+
+如不需请留空，注意不要将其前面的`,`删除了。
+
+![](assets/img/添加Secrets.gif)
+
+## 4.第一次运行actions
+
+只需要一个`push`操作就会触发`actions`运行
+
+将项目`poinMe.txt`文件中的`flag`值由`0`改为`1`即可。
+
+```sh
+- flag: 0
++ flag: 1
+```
+
+![](assets/img/运行结果.gif)
+
+## 大功告成
+
+以后每天会在8:30进行自动签到。
+
+## 参考项目
+
+[HAUT_autoCheck](https://github.com/YooKing/HAUT_autoCheck)
+
+[wanmei_campus](https://github.com/zhongbr/wanmei_campus)
---
+++ main.py
@@ -0,0 +1,235 @@
+import time,json,requests,random,datetime,os,sys
+from campus import CampusCard
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+
+def main():
+    #sectets字段录入
+    text, sckey, success, failure, result, phone, password,guardianPhone = [], [], [], [], [], [], [], []
+    #多人循环录入
+    while True:  
+        try:
+            users = input()
+            info = users.split(',')
+            phone.append(info[0])
+            password.append(info[1])
+            text.append(info[2])
+            guardianPhone(info[3])
+            sckey.append(info[4])
+        except:
+            break
+
+    #提交打卡
+    for index,value in enumerate(phone):
+        print("开始获取用户%sDeptId"%(value[-4:]))
+        count = 0
+        while (count < 3):
+            try:
+                campus = CampusCard(phone[index], password[index])
+                loginJson = campus.get_main_info()
+                token = campus.user_info["sessionId"]
+                stuNum = loginJson["outid"]
+                userName = loginJson["name"]  
+                driver.get('https://reportedh5.17wanxiao.com/collegeHealthPunch/index.html?token=%s#/punch?punchId=180'%token)
+                #time.sleep(10)
+                response = check_in(text[index],stuNum,userName,phone[index],guardianPhone[index],token)
+                if  response.json()["msg"] == '成功'and index == 0:
+                    strTime = GetNowTime()
+                    success.append(value[-4:])
+                    print(response.text)
+                    msg = value[-4:]+"打卡成功-" + strTime
+                    result=response
+                    break
+                elif response.json()["msg"] == '业务异常'and index == 0:
+                    strTime = GetNowTime()
+                    failure.append(value[-4:])
+                    print(response.text)
+                    msg = value[-4:]+"打卡失败-" + strTime
+                    result=response
+                    count = count + 1
+                elif response.json()["msg"] == '成功':
+                    strTime = GetNowTime()
+                    success.append(value[-4:])
+                    print(response.text)
+                    msg = value[-4:]+"打卡成功-" + strTime
+                    break
+                else:
+                    strTime = GetNowTime()
+                    failure.append(value[-4:])
+                    print(response.text)
+                    msg = value[-4:] + "打卡异常-" + strTime
+                    count = count + 1
+                    print('%s打卡失败，开始第%d次重试...'%(value[-6:],count))
+                    time.sleep(15)
+        
+            except:
+                msg = "出现错误"
+                failure.append(value[-4:])
+                break
+        print(msg)
+        print("-----------------------")
+    fail = sorted(set(failure),key=failure.index)
+    strTime = GetNowTime()
+    title = "成功: %s 人,失败: %s 人"%(len(success),len(fail))
+    try:
+        if  len(sckey[0])>2:
+            print('主用户开始微信推送...')
+            WechatPush(title,'https://sc.ftqq.com/'+sckey[0]+'.send',success,fail,result)
+    except:
+        print("微信推送出错！")
+#时间函数
+def GetNowTime():
+    cstTime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+    strTime = cstTime.strftime("%H:%M:%S")
+    return strTime
+
+#班级获取函数
+def GetDeptId(text):
+    try:
+        TextStr = text.split('-', 3)
+        ClassName = TextStr[2] 
+    # 获取deptId
+    except:
+        print("获取失败，请检查格式")
+    try:
+        for Class in AllClass:
+            if (Class['deptName'] == ClassName):
+                deptId = Class['deptid']
+        if deptId:
+            print('获取deptId成功!')
+    except:
+        print("获取deptId失败！")
+        exit(1)
+    return deptId
+
+#打卡参数配置函数
+def GetUserJson(deptId,text,stuNum,userName,phone,guardianPhone,token):
+    return  {
+	"businessType": "epmpics",
+	"method": "submitUpInfo",
+	"jsonData": {
+		"deptStr": {
+			"deptid": deptId,
+			"text": text
+		},
+		"areaStr": {"streetNumber":"","street":"新七大道","district":"浉河区","city":"信阳市","province":"河南省","town":"","pois":"信阳师范学院","lng":114.04613300000214 + random.random()/100000,"lat":32.15061303752275 + random.random()/100000,"address":"浉河区新七大道信阳师范学院","text":"河南省-信阳市","code":""},
+		"reportdate": round(time.time()*1000),
+		"customerid": "5920",
+		"deptid": deptId,
+		"source": "app",
+		"templateid": "pneumonia",
+		"stuNo": stuNum,
+		"username": userName,
+		"phonenum": phone,
+		"userid": round(time.time()),
+		"updatainfo": [{
+			"propertyname": "wendu",
+			"value": round(random.uniform(36.2,36.8),1)
+		}, {
+			"propertyname": "symptom",
+			"value": "无症状"
+		}, {
+			"propertyname": "jkzks",
+			"value": "正常"
+		}, {
+			"propertyname": "jtcy",
+			"value": "否"
+		}, {
+			"propertyname": "SFJCQZHYS",
+			"value": "否"
+		}, {
+			"propertyname": "sfddgr",
+			"value": "否"
+		}, {
+			"propertyname": "isTouch",
+			"value": "否"
+		}, {
+			"propertyname": "是否途径或逗留过疫情中，高风险地区？",
+			"value": ""
+		}, {
+			"propertyname": "isAlreadyInSchool",
+			"value": "有"
+		}, {
+			"propertyname": "hsjc0511",
+			"value": "否"
+		}, {
+			"propertyname": "ownPhone",
+			"value": phone
+		}, {
+			"propertyname": "emergencyContact",
+			"value": "监护人"
+		}, {
+			"propertyname": "mergencyPeoplePhone",
+			"value": guardianPhone
+		}],
+		"gpsType": 0,
+		"token": token
+	}
+}
+
+#打卡提交函数
+def check_in(text,stuNum,userName,phone,guardianPhone,token):
+    deptId = GetDeptId(text)
+    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+    jsons=GetUserJson(deptId,text,stuNum,userName,phone,guardianPhone,token)
+    #提交打卡
+    response = requests.post(sign_url, json=jsons,)
+    return response
+
+#json读取函数
+def GetFromJSON(filename): 
+    flag = False
+    idStr={} 
+    try:
+        j_file=open(filename,'r', encoding='utf8')
+        idStr=json.load(j_file)
+        flag=True
+    except:
+        print('从%s读取JSON数据出错！'%filename)
+    finally:
+        if flag:
+            j_file.close()
+
+        
+    return idStr
+
+#微信通知
+def WechatPush(title,sckey,success,fail,result):    
+    strTime = GetNowTime()
+    page = json.dumps(result.json(), sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
+    content = f"""
+`{strTime}` 
+#### 打卡成功用户：
+`{success}` 
+#### 打卡失败用户:
+`{fail}`
+#### 主用户打卡信息:
+```
+{page}
+```
+        """
+    data = {
+            "text":title,
+            "desp":content
+    }
+    try:
+        req = requests.post(sckey,data = data)
+        if req.json()["errmsg"] == 'success':
+            print("Server酱推送服务成功")
+        else:
+            print("Server酱推送服务失败")
+    except:
+        print("微信推送参数错误")
+if __name__ == '__main__':
+    filename = r'text.json'
+    AllClass = GetFromJSON(filename)['data']['majorAll']
+    main()
